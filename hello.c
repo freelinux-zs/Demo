31d661702f7a77bd6246294656cdb8b7719f8b7c
xuzhoubin <zhoubin.xu@foxmail.com>
2018-07-17 15:46:09 +0800

    feat(linux驱动Demo):drv linux drv hello world demo.

---
+++ hello.c
@@ -0,0 +1,25 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+
+
+static int hello_init(void)
+{
+	printk("hello world enter\r\n");
+	return 0;
+}
+
+static void hello_exit(void)
+{
+	printk("hello world exit\n");
+}
+
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+
+MODULE_AUTHOR("Zac <zhoubin.xu@gmail.com>");
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_DESCRIPTION("A simple Hello world Module");
+MODULE_ALIAS("a simplest module");
